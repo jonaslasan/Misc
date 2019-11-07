51271d6562cc6be816dbc498be3fce244c65e75f
Desktop-home\jonas <jonas.lasan@gmail.com>
2019-11-07 23:04:17 +0100

    Java program to draw astroid curve in the console.

---
+++ Astroid/Astroid.java
@@ -0,0 +1,47 @@
+public class Astroid {
+	public static void main (String[] args) {
+		
+		double a = 20;	
+		double epsilon = 0.3;
+		
+		// Optional parameters: a, ε
+		if (args.length > 0)
+		{
+			a = Integer.parseInt(args[0]);
+		}
+		if (args.length > 1)
+		{
+			epsilon = Double.parseDouble(args[1]);
+		}
+		
+		int width = (int)(a * 2);
+		int height = (int)(a * 2);
+		
+		for (int y = height/2; y > (-height/2); y--) {
+			for (int x = (-width/2); x < width/2; x++) {
+				// Check if x^(2/3) + y^(2/3) = a
+				double left = Math.pow(Math.abs(x), (double)2/3) + Math.pow(Math.abs(y), (double)2/3);
+				double right = Math.pow(a, (double)2/3);
+				
+				// Check if it fits inside the current square
+				double delta = Math.abs(left - right);
+				if (delta < epsilon) {
+					System.out.print("*");
+				}
+				else {
+					if (y == 0) {
+						System.out.print("-");	
+						continue;
+					}
+					if (x == 0) {
+						System.out.print("|");
+						continue;
+					}
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.println();
+		}
+	}
+}
---
+++ Astroid/README.md
@@ -0,0 +1,19 @@
+# Astroid
+Curve studied by Rømer in 1674, Jean Bernoulli in 1691, Leibniz in 1715 and D'Alembert in 1748.
+Named by Littrow in 1838.
+
+"Astroid" comes from the Greek word "star" : "astron" (like the word "asteroid" designing star like objects).
+
+Other names : H4 (hypocycloid with 4 cusps), cubocycloid.
+
+## Draw an Astroid in the console
+Optional arguments:
+- a
+- ε
+
+Example output:
+![output](output.png)
+
+What we expect:
+
+![expectation](expectation.png)
